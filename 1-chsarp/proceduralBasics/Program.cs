78b65d2e9f1d3f9a72d4a3c17d84af994d618fa7
arman <arman97rm@gmail.com>
2020-06-16 16:14:48 -0400

    first commit

---
+++ 1-chsarp/proceduralBasics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proceduralBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
